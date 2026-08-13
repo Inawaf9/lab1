852f9dd4b3f7b8b58b3cfc8539a008583072d994
Nawaf Abdullah <henawaf9@gmail.com>
2026-08-13 13:00:01 +0300

    Lab1

--- src/Main.java
+++ src/Main.java
@@ -213,7 +213,7 @@ public class Main {
          int minutes = (seconds /60) /24;
          int second = ((seconds /(60 * 24)));
 
-        System.out.println(hours + "," + minutes + "," + second);
+        System.out.println(hours + ":" + minutes + ":" + second);
 
 
         System.out.println("=============================");
